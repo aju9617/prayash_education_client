0876b2c6b061ee3ce75053875ffd4f7c2c37f00c
ashutosh <ashusinghweb414@gmail.com>
2022-09-28 14:18:01 +0530

    checking prod

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,14 +1,14 @@
 export const constant = {
   TOKEN_KEY: "id738_u839",
-  RAZORPAY_KEY: "rzp_test_SbTWDfLsBrSMTm",
+  RAZORPAY_KEY: "rzp_test_OsfwP2faDU2JQS",
   //   ------------ local -------------------------//
-  API_URL: "http://localhost:4040/api/v1",
-  MEDIA_URL: "http://localhost:4040/api/v1/media",
-  CLIENT_URL: "https://localhost:3000",
+  // API_URL: "http://localhost:4040/api/v1",
+  // MEDIA_URL: "http://localhost:4040/api/v1/media",
+  // CLIENT_URL: "https://localhost:3000",
 
   // ------------ production -------------------------//
-  // API_URL: "",
-  // MEDIA_URL: "",
+  API_URL: "https://api.prayasheducation.com/api/v1",
+  MEDIA_URL: "https://api.prayasheducation.com/api/v1",
 };
 
 export default constant;

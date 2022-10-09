495f8fc0ea507e912b5006f322923928612986cb
ashutosh <ashusinghweb414@gmail.com>
2022-10-09 19:06:13 +0530

    updating razor pay key

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,13 +1,14 @@
 export const constant = {
   TOKEN_KEY: "id738_u839",
-  RAZORPAY_KEY: "rzp_test_OsfwP2faDU2JQS",
 
   //   ------------ local -------------------------//
   // API_URL: "http://localhost:4040/api/v1",
   // MEDIA_URL: "http://localhost:4040/api/v1/media",
   // CLIENT_URL: "https://localhost:3000",
+  // RAZORPAY_KEY: "rzp_test_OsfwP2faDU2JQS",
 
   // ------------ production -------------------------//
+  RAZORPAY_KEY: "rzp_live_1jmuacVeVAn4GN",
   API_URL: "https://api.prayasheducation.com/api/v1",
   MEDIA_URL: "https://api.prayasheducation.com/api/v1",
 };
